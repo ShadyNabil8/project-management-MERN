fa55123367f0a9d6d5b8873d437d42abfe661dd1
shady <shadyngheith@gmail.com>
2024-09-01 11:44:30 +0300

    Make the loading page absolute to avoid overflow

--- frontend/src/components/MainLoading.jsx
+++ frontend/src/components/MainLoading.jsx
@@ -3,12 +3,12 @@ import { developerImage } from "../assets/images";
 
 const MainLoading = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center overflow-hidden bg-white">
-      <span className="animation-delay-100 mr-6 size-14 animate-bounce rounded-full bg-[#FF7120]"></span>
-      <span className="animation-delay-200 mr-6 size-14 animate-bounce rounded-full bg-[#FF8E07]"></span>
-      <span className="animation-delay-300 mr-6 size-14 animate-bounce rounded-full bg-[#FF4D41]"></span>
-      <span className="animation-delay-400 mr-6 size-14 animate-bounce rounded-full bg-[#FF2861]"></span>
-      <span className="animation-delay-500 mr-6 size-14 animate-bounce rounded-full bg-[#FF0082]"></span>
+    <div className="absolute z-10 flex h-screen w-screen items-center justify-center overflow-hidden bg-white">
+      <span className="mr-6 size-14 animate-bounce rounded-full bg-[#FF7120] animation-delay-100"></span>
+      <span className="mr-6 size-14 animate-bounce rounded-full bg-[#FF8E07] animation-delay-200"></span>
+      <span className="mr-6 size-14 animate-bounce rounded-full bg-[#FF4D41] animation-delay-300"></span>
+      <span className="mr-6 size-14 animate-bounce rounded-full bg-[#FF2861] animation-delay-400"></span>
+      <span className="mr-6 size-14 animate-bounce rounded-full bg-[#FF0082] animation-delay-500"></span>
     </div>
   );
 };
