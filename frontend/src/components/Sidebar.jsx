ff4966f086ddeddbf4c7016d8d9e43564942961e
shady <shadyngheith@gmail.com>
2024-09-08 08:51:48 +0300

    Change Sidebar icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,27 +4,34 @@ import SidebarNavigator from "./SidebarNavigator";
 import Spaces from "./Spaces";
 import SidebarLink from "./SidebarLink";
 import { useParams } from "react-router-dom";
+import { BiHomeAlt } from "react-icons/bi";
+import { GoInbox } from "react-icons/go";
+import { LuLayoutDashboard } from "react-icons/lu";
 
 const Sidebar = () => {
   const { workspaceId } = useParams();
 
   return (
-    <div className="h-full lg:overflow-auto border-r bg-[#F7F8F9]">
+    <div className="h-full border-r bg-[#F7F8F9] lg:overflow-auto">
       <SidebarNavigator>
         <SidebarLink
-          link={{ title: "Home", image: homeImage, to: `/${workspaceId}/home` }}
+          link={{
+            title: "Home",
+            image: <BiHomeAlt className="size-5 text-gray-600" />,
+            to: `/${workspaceId}/home`,
+          }}
         />
         <SidebarLink
           link={{
             title: "Inbox",
-            image: inboxImage,
+            image: <GoInbox className="size-5 text-gray-600" />,
             to: `/${workspaceId}/inbox`,
           }}
         />
         <SidebarLink
           link={{
             title: "Dashboards",
-            image: dashboardImage,
+            image: <LuLayoutDashboard className="size-5 text-gray-600" />,
             to: `/${workspaceId}/dashboards`,
           }}
         />
--- frontend/src/components/SidebarLink.jsx
+++ frontend/src/components/SidebarLink.jsx
@@ -7,9 +7,7 @@ const SidebarLink = ({ link }) => {
       to={link.to}
       className="flex cursor-pointer items-center justify-center rounded-md p-2 hover:bg-gray-200"
     >
-      <span className="shrink-0">
-        <img className="size-[15px]" src={link?.image}></img>
-      </span>
+      <span className="shrink-0">{link.image}</span>
       <span className="ml-3 hidden text-sm text-gray-800 lg:block">
         {link.title}
       </span>
