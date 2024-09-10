8d30cb29c1349786502a749616a0889fc8f1e776
shady <shadyngheith@gmail.com>
2024-09-10 11:32:20 +0300

    Chage used states in login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { Form, useNavigate } from "react-router-dom";
+import { Form, useNavigate, Link } from "react-router-dom";
 import { authService } from "../api";
 import { MdOutlineEmail } from "react-icons/md";
 import { SlLock } from "react-icons/sl";
@@ -18,7 +18,7 @@ const LoginPage = () => {
 
       setLoading(true);
 
-      const userData = await authService(username, password);
+      const userData = await authService(loginData.email, loginData.password);
 
       if (userData) {
         login(userData);
@@ -30,9 +30,6 @@ const LoginPage = () => {
     }
   };
 
-  useEffect(() => {
-    console.log(loginData);
-  }, [loginData]);
   if (loading) {
     return <div>Loading ...</div>;
   }
@@ -40,9 +37,15 @@ const LoginPage = () => {
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-[#FAFBFC]">
       <div className="absolute -bottom-[55%] -left-20 -right-60 h-screen -rotate-[10deg] rounded-full bg-gradient-to-l from-[#FC466B] to-[#3F5EFB]"></div>
-      <Form className="absolute -right-1/2 left-1/2 flex h-[460px] w-[347px] -translate-x-1/2 translate-y-1/2 flex-col items-center rounded-lg bg-white px-[22px] py-10 shadow-3xl md:w-[480px] md:px-16">
+      <Form
+        className="absolute -right-1/2 left-1/2 flex h-[460px] w-[347px] -translate-x-1/2 translate-y-1/2 flex-col items-center rounded-lg bg-white px-[22px] py-10 shadow-3xl md:w-[480px] md:px-16"
+        onSubmit={handleLogin}
+      >
         <p className="text-3xl font-bold text-gray-800">Welcome back!</p>
-        <button className="relative mt-3 flex h-[40px] w-full items-center justify-center rounded-md border p-2 transition-colors hover:bg-[#f7f9fc]">
+        <button
+          className="relative mt-3 flex h-[40px] w-full items-center justify-center rounded-md border p-2 transition-colors hover:bg-[#f7f9fc]"
+          type="button"
+        >
           <span className="justify-self-center text-gray-700">
             Continue with Google
           </span>
@@ -84,9 +87,18 @@ const LoginPage = () => {
             }
           ></input>
         </div>
-        <button className="mt-7 h-[50px] w-full rounded-lg bg-[#5F55EE] font-medium text-white transition-colors hover:bg-[#544DC9]">
+        <button
+          className="mt-7 h-[50px] w-full rounded-lg bg-[#5F55EE] font-medium text-white transition-colors hover:bg-[#544DC9]"
+          type="submit"
+        >
           Log in
         </button>
+        <div className="absolute -bottom-12 text-sm text-white">
+          <span className="text-sm text-white">Don't have an account? </span>
+          <Link className="border-b border-b-[#bababa] transition-colors hover:border-b-white">
+            Sign up
+          </Link>
+        </div>
       </Form>
     </div>
   );
